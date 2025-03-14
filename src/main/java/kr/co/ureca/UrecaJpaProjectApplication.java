2fe28bdfc9f2c6fb783a89414d9aeccb389e0e22
yereumi <lovelyyeji01@gmail.com>
2025-03-14 16:32:47 +0900

    Initial commit

---
+++ src/main/java/kr/co/ureca/UrecaJpaProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ureca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrecaJpaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrecaJpaProjectApplication.class, args);
+    }
+
+}
